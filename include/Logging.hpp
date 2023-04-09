859c5d7ac76e910cf943feb4aacbbd0d3854294e
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-09 22:55:02 +0100

    Add logging to `main.cpp`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ include/Logging.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef LOGGING_h
+
+#ifndef ESP_LOG_H
+#include "esp_log.h"
+#endif
+
+#endif
--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include "Logging.hpp"
+
+#define TAG "app_main"
 
 // Enabling C++ compile
-extern "C" { void app_main(); }
+extern "C"
+{
+    void app_main();
+}
 
 void app_main(void)
 {
-
+    ESP_LOGI(TAG, "Booting up and initializing application...");
 }
